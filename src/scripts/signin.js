542b4212e329ae680688be181a37b003cc68cde5
Emingy <52676421+Shawk-hub@users.noreply.github.com>
2021-02-16 00:40:46 +0300

    fix indexedDB signin

--- src/scripts/signin.js
+++ src/scripts/signin.js
@@ -1,5 +1,6 @@
     
 function signIn (email, pass) {
+    
     let alert = document.getElementById('b-form-alert');
     alert.classList.add("d-none");
     if(email != '' && pass != ''){
@@ -9,36 +10,63 @@ function signIn (email, pass) {
                     email: email,
                     pass: value
                 }
-        
-                let response = fetch('../php/signin.php', {
-                    method: 'POST',
-                    headers: {
-                      'Content-Type': 'application/json;charset=utf-8'
-                    },
-                    body: JSON.stringify(auth)
-                  });
-                response.then(function(result){
-                    result.text().then(function(res){
-                        if(JSON.parse(res).status == 'Sign In'){
-                            res = JSON.parse(res);
-                            sessionStorage.setItem('name', res.name);
-                            sessionStorage.setItem('token', res.token);
-                            window.location.href = "../../";
-                        }else if (res == 'Empty'){
-                            alert.innerHTML = 'Неправильно введен логин или пароль';
-                            alert.classList.remove("d-none");
-                        }else if (res == 'Error'){
-                            alert.innerHTML = 'Ошибка запроса';
-                            alert.classList.remove("d-none");
-                        }else{
-                            console.error('Error',res)
+                window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB
+                let openRequest = indexedDB.open("db", 1);
+                openRequest.onerror = (e) => {
+                    console.log('Error', e.target.errorCode);
+                }
+                openRequest.onsuccess = (e) => {
+                    console.log('Success')
+                    var db = e.target.result;
+                    var transaction = db.transaction('auth', 'readwrite')
+                    var auth = transaction.objectStore('auth')
+                    // let a = {
+                    //     login: 'test@mail.ru',
+                    //     pass: 'test'
+                    // }
+                    // let request = auth.add(a);
+                    // console.dir(request)
+                    var authentication = () =>{
+                        var request = auth.openCursor();
+                        request.onsuccess = (event) => {
+                            var cursor = event.target.result;
+                            if (cursor) {
+                                if (cursor.value.login.indexOf(email) !== -1 && cursor.value.pass.indexOf(value) !== -1) {
+                                    return 'true';
+                                }
+                        
+                                cursor.continue();          
+                            }else{
+                                return 'error';
+                            }
+                            return 'false';
                         }
-                    })
-                })
-                response.catch(function(error){
-                    alert.innerHTML = 'Error'+error;
-                    alert.classList.remove("d-none");
-                })
+                    }
+                    if (authentication()=='true'){
+                        var cursor = event.target.result;
+                        sessionStorage.setItem('name', cursor.value.name);
+                        sessionStorage.setItem('token', cursor.value.token);
+                        window.location.href = "../../index.html";
+                    }else if(authentication()=='error'){
+                        alert.innerHTML = 'Ошибка';
+                        alert.classList.remove("d-none");
+                    }else if(authentication()=='false'){
+                        alert.innerHTML = 'Неправильно введен логин или пароль';
+                        alert.classList.remove("d-none");
+                    }
+                    // request.onsuccess = () => {
+                    //     console.log('Записано')
+                    // }
+                    // request.onerror = (e) => {
+                    //     console.log('Ошибка при записи', e.target.error)
+                    // }
+                }
+                openRequest.onupgradeneeded = () => {
+                    console.log('onupgradeneeded')
+                    if (!openRequest.result.objectStoreNames.contains('auth')){
+                        openRequest.result.createObjectStore('auth', {keyPath: 'id', autoIncrement: true})
+                    }
+                }
             }).catch(function(error){
                 alert.innerHTML = 'Error'+error;
                 alert.classList.remove("d-none");
--- src/scripts/signup.js
+++ src/scripts/signup.js
@@ -1,4 +1,11 @@
 function signup (name, email, pass) {
+    var rand = function() {
+        return Math.random().toString(36).substr(2); // remove `0.`
+    };
+    
+    var token = function() {
+        return rand() + rand(); // to make it longer
+    };
     let alert = document.getElementById('b-form-alert');
     alert.classList.add("d-none");
     alert.classList.remove("alert-success");
@@ -9,38 +16,98 @@ function signup (name, email, pass) {
                 let reg = {
                     name: name,
                     email: email,
-                    pass: value
+                    pass: value,
+                    token: token()
                 }
-        
-                let response = fetch('../php/signup.php', {
-                    method: 'POST',
-                    headers: {
-                      'Content-Type': 'application/json;charset=utf-8'
-                    },
-                    body: JSON.stringify(reg)
-                  });
-                response.then(function(result){
-                    result.text().then(function(res){
-                        if(res == 'Signup'){
+                window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB
+                let openRequest = indexedDB.open("db", 1);
+                openRequest.onerror = (e) => {
+                    console.log('Error', e.target.errorCode);
+                }
+                openRequest.onsuccess = (e) => {
+                    console.log('Success')
+                    var db = e.target.result;
+                    var transaction = db.transaction('auth', 'readwrite')
+                    var auth = transaction.objectStore('auth')
+                    var authentication = () =>{
+                        var request = auth.openCursor();
+                        request.onsuccess = (event) => {
+                            var cursor = event.target.result;
+                            if (cursor && cursor!=null) {
+                                if (cursor.value.login.indexOf(email) !== -1) {
+                                    return 'true';
+                                }
+                        
+                                cursor.continue();          
+                            }else if(cursor==null){
+                                return 'false';
+                            }else{
+                                return 'error';
+                            }
+                            return 'false';
+                        }
+                        request.onerror = () => {
+                            console.log('error')
+                        }
+                    }
+                    if (authentication()=='true'){
+                        alert.innerHTML = 'Аккаунт уже существует';
+                        alert.classList.remove("d-none");
+                    }else if(authentication()=='error'){
+                        alert.innerHTML = 'Ошибка';
+                        alert.classList.remove("d-none");
+                    }else if(authentication()=='false'){
+                        console.log(authentication())
+                        let request = auth.add(reg);
+                        console.dir(request)
+                        request.onsuccess = () => {
                             alert.innerHTML = 'Аккаунт успешно зарегистрирован';
                             alert.classList.remove("alert-danger");
                             alert.classList.add("alert-success");
                             alert.classList.remove("d-none");
-                        }else if (res == 'Busy'){
-                            alert.innerHTML = 'Аккаунт уже существует';
-                            alert.classList.remove("d-none");
-                        }else if (res == 'Error'){
-                            alert.innerHTML = 'Ошибка запроса';
-                            alert.classList.remove("d-none");
-                        }else{
-                            console.error('Error',res)
                         }
-                    })
-                })
-                response.catch(function(error){
-                    alert.innerHTML = 'Error'+error;
-                    alert.classList.remove("d-none");
-                })
+                        request.onerror = (e) => {
+                            alert.innerHTML = 'Ошибка';
+                            alert.classList.remove("d-none");
+                        }
+                    }
+                    console.log(authentication())
+                }
+                openRequest.onupgradeneeded = () => {
+                    console.log('onupgradeneeded')
+                    if (!openRequest.result.objectStoreNames.contains('auth')){
+                        openRequest.result.createObjectStore('auth', {keyPath: 'id', autoIncrement: true})
+                    }
+                }
+                // let response = fetch('../php/signup.php', {
+                //     method: 'POST',
+                //     headers: {
+                //       'Content-Type': 'application/json;charset=utf-8'
+                //     },
+                //     body: JSON.stringify(reg)
+                //   });
+                // response.then(function(result){
+                //     result.text().then(function(res){
+                //         if(res == 'Signup'){
+                //             alert.innerHTML = 'Аккаунт успешно зарегистрирован';
+                //             alert.classList.remove("alert-danger");
+                //             alert.classList.add("alert-success");
+                //             alert.classList.remove("d-none");
+                //         }else if (res == 'Busy'){
+                //             alert.innerHTML = 'Аккаунт уже существует';
+                //             alert.classList.remove("d-none");
+                //         }else if (res == 'Error'){
+                //             alert.innerHTML = 'Ошибка запроса';
+                //             alert.classList.remove("d-none");
+                //         }else{
+                //             console.error('Error',res)
+                //         }
+                //     })
+                // })
+                // response.catch(function(error){
+                //     alert.innerHTML = 'Error'+error;
+                //     alert.classList.remove("d-none");
+                // })
             }).catch(function(error){
                 alert.innerHTML = 'Error'+error;
                 alert.classList.remove("d-none");
